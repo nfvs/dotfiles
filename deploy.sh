03ede49d6fa290bfe88df6c173d60ea3bbe22770
Nuno Santos <nunofvsantos@gmail.com>
2012-11-18 18:46:06 +0100

    changed deploy script to create symbolic links instead of copying files

--- deploy.sh
+++ deploy.sh
@@ -3,37 +3,39 @@
 CP="cp -R"
 
 FILES=(
-	"_vim/"
+	"_vim"
 	"_vimrc"
 	"_gvimrc"
 	"_gitconfig"
 	"_screenrc")
 
 DESTFILES=(
-	".vim/"
+	".vim"
 	".vimrc"
 	".gvimrc"
 	".gitconfig"
 	".screenrc")
 
-echo "Deploying '${FILES[@]}' to home directory."
+echo "Deploying '${DESTFILES[@]}' to home directory."
 
 for f in "${!FILES[@]}" # iterate index, not values
 do
+
+	SRCFILE="${PWD}/${FILES[$f]}"
 	DESTFILE="$HOME/${DESTFILES[$f]}"
 	
-	# check if destination is a folder, and already exists
-	# cp -i will ask for overwrite automatically for files
-	if [[ -d "$DESTFILE" && -e "$DESTFILE" ]]
+	if [[ -h "$DESTFILE" || -e "$DESTFILE" ]]
 	then
-		read -p "overwrite directory $DESTFILE}? (y/n [n]) "
+		read -p "overwrite file $DESTFILE? (y/n [n]) "
 		if [[ "$REPLY" == "y" || "$REPLY" == "Y" ]]
 		then
-			cp -r "${FILES[$f]}" "$DESTFILE}"
+			ln -sf "$SRCFILE" "$DESTFILE"
 		else
 			echo "not overwritten"
 		fi
+
 	else
-		cp -i "${FILES[$f]}" "$DESTFILE"
+		ln -s "$SRCFILE" "$DESTFILE"
 	fi
+	
 done
