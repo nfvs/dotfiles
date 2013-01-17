913251c8bbbaad1f0a7fc3ae9e63ad76368ed582
Nuno Santos <nunofvsantos@gmail.com>
2013-01-17 20:34:17 +0100

    Added Sublime Text 2 configuration (and added to deploy)

--- README.md
+++ README.md
@@ -1,24 +1,40 @@
-Virtual Home  
-============
+# Virtual Home
 
 Personal configurations files and tweaks.
+
+
+## Applications
+
+### Sublime Text 2
+ * `sublime-text-2` directory with user configuration including packages, syntax-specific configuration, etc.
+
+_Target directory:_
+ * OS X: `~/Library/Application Support/Sublime Text 2/Packages/User`
+ * Linux: `~/.config/sublime-text-2/Packages/User`
+ * Windows: `%APPDATA%\Sublime Text 2\Packages\User`
 
 ### Vim
  * `_vim/` directory with plugins and filetype-based syntax configuration
  * `_vimrc` Vim configuration
  * `_gvimrc` Vim GUI configuration
 
+_Target directory:_ `~/`
+
 ### Git
  * `_gitconfig` git global configuration, name, email
+
+_Target directory:_ `~/`
 
 ### GNU Screen
  * `_screenrc` screen configuration
 
-Deployment
-------------
+_Target directory:_ `~/`
+
+
+## Deployment
 
 ### OS X / Unix
-Run `deploy.sh` to copy all configuration files to the home directory (`~/`). 
+Run `deploy.sh` to copy all configuration files to the target directories. 
 
 ### Windows
 _TODO_
--- deploy.sh
+++ deploy.sh
@@ -16,15 +16,66 @@ DESTFILES=(
 	".gitconfig"
 	".screenrc")
 
-echo "Deploying '${DESTFILES[@]}' to home directory."
+# Will hold full filenames
+FILES_FULL=()
+DESTFILES_FULL=()
 
+
+# Destination directories relative to Home (~/)
+SUBLIME_TEXT_2="sublime-text-2"
+SUBLIME_TEXT_2_DEST_OSX="Library/Application Support/Sublime Text 2/Packages/User"
+SUBLIME_TEXT_2_DEST_UNIX=".config/sublime-text-2/Packages/User"
+
+
+# 1) Detect platform, choose correct SUBLIME_TEXT_2_DEST
+echo ""
+platform='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+	echo "Platform detected: Linux"
+	SUBLIME_TEXT_2_DEST=$SUBLIME_TEXT_2_DEST_UNIX
+elif [[ "$unamestr" == 'Darwin' ]]; then
+	echo "Platform detected: OS X"
+   platform='osx'
+   SUBLIME_TEXT_2_DEST=$SUBLIME_TEXT_2_DEST_OSX
+else
+	echo "Unknown platform, exiting."
+	exit 0
+fi
+
+# 2) Print files to be deployed, prompt to continue
+echo "Deploying:"
 for f in "${!FILES[@]}" # iterate index, not values
 do
+	SRCFILE="${FILES[$f]}";
+	DESTFILE="~/${DESTFILES[$f]}";
+	echo -e "  ${SRCFILE}  \t --> ${DESTFILE}";
+done
+echo -e "  $SUBLIME_TEXT_2 --> ~/$SUBLIME_TEXT_2_DEST";
+echo ""
 
-	SRCFILE="${PWD}/${FILES[$f]}"
-	DESTFILE="$HOME/${DESTFILES[$f]}"
-	
-	if [[ -h "$DESTFILE" || -e "$DESTFILE" ]]
+read -p "Continue? (y/n [n]) "
+if [[ "$REPLY" != "y" && "$REPLY" != "Y" ]]
+then
+	echo "Aborted."
+	exit 0
+fi
+
+# 3) Build lists with full filenames
+for f in "${!FILES[@]}" # iterate index, not values
+do
+	FILES_FULL+=("$PWD/${FILES[$f]}")
+	DESTFILES_FULL+=("$HOME/${DESTFILES[$f]}")
+done
+FILES_FULL+=("$PWD/$SUBLIME_TEXT_2")
+DESTFILES_FULL+=("$HOME/$SUBLIME_TEXT_2_DEST")
+
+# 4) Deploy!
+for f in "${!FILES_FULL[@]}" # iterate index, not values
+do
+	SRCFILE="${FILES_FULL[$f]}"
+	DESTFILE="${DESTFILES_FULL[$f]}"
+	if [[ -h "${DESTFILE}" || -e "${DESTFILE}" ]]
 	then
 		read -p "overwrite file $DESTFILE? (y/n [n]) "
 		if [[ "$REPLY" == "y" || "$REPLY" == "Y" ]]
@@ -33,9 +84,7 @@ do
 		else
 			echo "not overwritten"
 		fi
-
 	else
 		ln -s "$SRCFILE" "$DESTFILE"
 	fi
-	
 done
