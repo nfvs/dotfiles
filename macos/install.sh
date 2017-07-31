936a30565c72ae8ee94ad948b7c5e6af9aece236
Nuno Santos <nunofvsantos@gmail.com>
2017-07-31 21:18:50 +0200

    Added oceanicnext iterm theme.

--- macos/install.sh
+++ macos/install.sh
@@ -9,8 +9,9 @@ DOTFILES_ROOT=$(pwd -P)
 source $DOTFILES_ROOT/zsh/functions.zsh
 
 # Install the Solarized Dark theme for iTerm
-for src in $DOTFILES_ROOT/macos/*.itermcolors
-do
-	open "${src}"
-	echo_success "Installed iTerm2 theme $(basename "${src}")"
-done
+# theme=base16-oceanicnext.dark
+# for src in $DOTFILES_ROOT/macos/$theme=*.itermcolors
+# do
+# 	open "${src}"
+# 	echo_success "Installed iTerm2 theme $(basename "${src}")"
+# done
