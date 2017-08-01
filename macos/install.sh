8bc63fb614605303a0636e4f7b7fae9d792a37c9
Nuno Santos <nunofvsantos@gmail.com>
2017-08-01 10:30:27 +0200

    Added macOS set-defaults. Moved macos themes, added terminal theme.

--- macos/install.sh
+++ macos/install.sh
@@ -10,8 +10,10 @@ source $DOTFILES_ROOT/zsh/functions.zsh
 
 # Install the Solarized Dark theme for iTerm
 # theme=base16-oceanicnext.dark
-# for src in $DOTFILES_ROOT/macos/$theme=*.itermcolors
+# for src in $DOTFILES_ROOT/macos/themes/$theme.itermcolors
 # do
 # 	open "${src}"
 # 	echo_success "Installed iTerm2 theme $(basename "${src}")"
 # done
+
+$DOTFILES_ROOT/macos/set-defaults.sh
