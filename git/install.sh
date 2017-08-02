73ef50e3723411723890e64fdccef4151164eced
Nuno Santos <nunofvsantos@gmail.com>
2017-08-02 09:12:17 +0200

    Fixed git symlinks, added sublime-babel.

--- git/install.sh
+++ git/install.sh
@@ -36,7 +36,7 @@ setup_gitconfig () {
         printf " - What is your github author email? "
         read -e git_authoremail
 
-        sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" $DOTFILES_ROOT/git/_gitconfig.local.example > $DOTFILES_ROOT/git/_gitconfig.local
+        sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" $DOTFILES_ROOT/git/gitconfig.local.example > $DOTFILES_ROOT/git/_gitconfig.local
     fi
 
     echo_success "gitconfig"
