19d543b0ef003757fb243d48f26bc932d4ef6ba3
Nuno Santos <nsantos@nvidia.com>
2018-10-19 16:00:51 +0200

    Updates

--- python/install.sh
+++ python/install.sh
@@ -1,12 +1,12 @@
-cd "$(dirname "$0")/.."
-DOTFILES_ROOT=$(pwd -P)
-HOME=~
+# cd "$(dirname "$0")/.."
+# DOTFILES_ROOT=$(pwd -P)
+# HOME=~
 
-python -m easy_install pip
-pip install pipsi
+# python -m easy_install pip
+# pip install pipsi
 
-pipsi install pew
-pipsi install pipenv
+# pipsi install pew
+# pipsi install pipenv
 
-# replace system python with homebrew's
-ln -sf /usr/local/bin/python2 /usr/local/bin/python
+# # replace system python with homebrew's
+# ln -sf /usr/local/bin/python2 /usr/local/bin/python
