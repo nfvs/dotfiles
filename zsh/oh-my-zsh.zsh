ed8af56de43ea4b3c391ed0cb7fa822bc144e225
Nuno Santos <nunofvsantos@gmail.com>
2019-06-17 14:54:41 +0200

    Vim FZF instead of CtrlP, oh-my-zsh kubectl plugin.

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -9,21 +9,22 @@ export ZSH=$HOME/.oh-my-zsh
 DEFAULT_USER=`whoami`
 
 plugins=(
-	git
-	osx
-	brew
-	node
-	npm
-	python
-	tmux
-	docker
-	pip
-	# virtualenv
-	# virtualenvwrapper
-	# ruby
-	# rvm
-	# rails
-	# bundler
+    git
+    osx
+    brew
+    node
+    npm
+    python
+    tmux
+    docker
+    pip
+    kubectl
+    # virtualenv
+    # virtualenvwrapper
+    # ruby
+    # rvm
+    # rails
+    # bundler
 )
 
 source $ZSH/oh-my-zsh.sh
