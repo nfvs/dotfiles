7861172412ae820680aaa83a074c36b230f52020
Nuno Santos <nsantos@nvidia.com>
2019-06-19 14:19:39 +0100

    tmux-new alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,5 +3,8 @@ alias reload!='. ~/.zshrc'
 alias cls='clear' # Good 'ol Clear Screen command
 alias venv='source ./.venv/bin/activate'
 
+# Start a new tmux session with 3 panes
+alias tmux-new='tmux new-session \; split-window -h \; split-window -v \; attach'
+
 # Perforce
 alias p4home="p4 -Ztag -F %clientRoot% info"
