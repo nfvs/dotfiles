45d226abb8f3337d613d176c057efbce52aa0081
Nuno Santos <nsantos@nvidia.com>
2022-07-13 08:14:38 +0100

    ZSH: add history incrementally and share it across sessions

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -25,8 +25,9 @@ setopt CORRECT
 setopt COMPLETE_IN_WORD
 # setopt IGNORE_EOF
 
-setopt APPEND_HISTORY # adds history
-# setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
+# setopt APPEND_HISTORY # adds history
+setopt INC_APPEND_HISTORY  # adds history incrementally...
+setopt SHARE_HISTORY  # ...and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
 
