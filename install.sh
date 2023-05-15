317aeb87199a02e5aab17b210212a7ef3a053ba0
Nuno Santos <nunofvsantos@gmail.com>
2023-05-15 15:55:56 +0200

    Fixed install functions

--- install.sh
+++ install.sh
@@ -12,8 +12,22 @@ if [[ ! -h "$DOTFILES_ROOT" ]]; then
   ln -sf "$(pwd -P)" "$DOTFILES_ROOT"
 fi
 
-# shellcheck source=bash/functions.sh
-source bash/functions.sh
+echo_info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+echo_user () {
+  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
+
+echo_success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+echo_fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+}
+
 
 link_file () {
   local src=$1 dst=$2
