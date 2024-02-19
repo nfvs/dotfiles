9a35d1e34161a9488d479906270654bfa39bd0c9
Nuno Santos <nunofvsantos@gmail.com>
2024-02-19 10:17:55 +0100

    Fish: history pager delete

--- _config/fish/config.fish
+++ _config/fish/config.fish
@@ -1,4 +1,17 @@
 if status is-interactive
+    # Keybindings
+
+    # Bind up/down to search prefix, like bash/zsh
+    # bind \e\[A history-prefix-search-backward
+    # bind \e\[B history-prefix-search-forward
+
+    # Shift delete: delete entry from Ctrl-R pager
+    # Also remove the "or backward-delete-char", otherwise arrows after deleting
+    # an entry will send an escape character.
+    # NOTE: For "history-pager-delete" to work with shift-delete in iTerm2,
+    # add the "Shift Del" Keyboard Shortcut with "Send Escape Sequence": "[3;2~"
+    bind -k sdc history-pager-delete
+
     # Don't exit on accidental Ctrl-D; exit on the third one!
     bind \cd delete-char
     bind \cd\cd\cd delete-or-exit
